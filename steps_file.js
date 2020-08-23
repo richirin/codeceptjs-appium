b576518d6cf1e82a437fc5bd6290d4f9b9615f90
richi <richi.ramadhan@fore.coffee>
2020-08-23 21:15:59 +0700

    update

--- steps_file.js
+++ steps_file.js
@@ -1,10 +1,20 @@
 // in this file you can append custom step methods to 'I' object
 
-module.exports = function() {
+module.exports = function () {
   return actor({
+    login: function (email, password) {
+      this.fillField('Masukkan Email / No Telepon Anda', email);
+      this.fillField('Masukan Password Anda', password);
+      this.tap('MASUK');
+      this.see('Pilih Outlet');
+    },
 
+    pilihOutlet: function (outlet) {
+      this.tap('#android:id/text1');
+      this.tap(outlet);
+      this.tap('SIMPAN');
+    },
     // Define custom steps here, use 'this' to access default methods of I.
     // It is recommended to place a general 'login' function here.
-
   });
-}
+};
--- tests/order.js
+++ tests/order.js
@@ -3,6 +3,7 @@ Feature('Order');
 let total;
 let bayar;
 let kembalian;
+let patt = /(0-9)/g;
 
 // Scenario('Transaksi terbuat saat klik bayar nanti di checkout page', async (I) => {
 //     await I.fillField('Masukkan Email / No Telepon Anda', process.env.EMAIL);
@@ -30,29 +31,84 @@ let kembalian;
 // });
 
 Scenario('Transaksi Menggunakan Cash', async (I) => {
-    await I.fillField('Masukkan Email / No Telepon Anda', process.env.EMAIL);
-    await I.fillField('Masukan Password Anda', process.env.PASSWORD);
-    await I.tap('MASUK');
-    await I.see('Pilih Outlet');
-    await I.tap('#android:id/text1');
-    await I.tap(process.env.OUTLET);
-    await I.tap('SIMPAN');
-    await I.waitForVisible('Home', 100)
-    await I.tap('Automation Kemeja');
-    await I.tap('SIMPAN')
-    await I.tap('BAYAR')
-    await I.tap(process.env.PELANGGAN)
-    await I.tap('SELANJUTNYA')
-    await I.see('Pilih Metode Pembayaran')
-    await I.see('Pembayaran Digital')
-    total = await I.grabTextFrom('#spe.pos.rewash:id/tv_payment_subtotal')
-    await I.tap('CASH')
-    await I.tap('10k')
-    bayar = parseInt(await I.grabTextFrom('#spe.pos.rewash:id/number_display').join(""))
-    await I.tap('SIMPAN')
-    await I.tap('KONFIRMASI')
-    kembalian = await I.grabTextFrom('#spe.pos.rewash:id/tvKembalian')
-    console.log(total, bayar, kembalian)
+  await I.login(process.env.EMAIL, process.env.PASSWORD);
+  await I.pilihOutlet(process.env.OUTLET);
+  await I.waitForVisible('Home', 100);
+  await I.tap('Automation Kemeja');
+  await I.tap('SIMPAN');
+  await I.tap('BAYAR');
+  await I.tap(process.env.PELANGGAN);
+  await I.tap('SELANJUTNYA');
+  await I.see('Pilih Metode Pembayaran');
+  await I.see('Pembayaran Digital');
+  total = await I.grabTextFrom('#spe.pos.rewash:id/tv_payment_subtotal')
+    .match(patt)
+    .join('');
+  await I.tap('CASH');
+  await I.tap('10k');
+  bayar = await I.grabTextFrom('#spe.pos.rewash:id/number_display')
+    .match(patt)
+    .join('');
+  await I.tap('SIMPAN');
+  await I.tap('KONFIRMASI');
+  kembalian = await I.grabTextFrom('#spe.pos.rewash:id/tvKembalian')
+    .match(patt)
+    .join('');
+  console.log(total, bayar, kembalian);
+});
+
+Scenario('Transaksi Menggunakan OVO', async (I) => {
+  await I.login(process.env.EMAIL, process.env.PASSWORD);
+  await I.pilihOutlet(process.env.OUTLET);
+  await I.waitForVisible('Home', 100);
+  await I.tap('Automation Kemeja');
+  await I.tap('SIMPAN');
+  await I.tap('BAYAR');
+  await I.tap(process.env.PELANGGAN);
+  await I.tap('SELANJUTNYA');
+  await I.see('Pilih Metode Pembayaran');
+  await I.see('Pembayaran Digital');
+  total = await I.grabTextFrom('#spe.pos.rewash:id/tv_payment_subtotal')
+    .match(patt)
+    .join('');
+  await I.tap('CASH');
+  await I.tap('10k');
+  bayar = await I.grabTextFrom('#spe.pos.rewash:id/number_display')
+    .match(patt)
+    .join('');
+  await I.tap('SIMPAN');
+  await I.tap('KONFIRMASI');
+  kembalian = await I.grabTextFrom('#spe.pos.rewash:id/tvKembalian')
+    .match(patt)
+    .join('');
+  console.log(total, bayar, kembalian);
+});
+
+Scenario('Transaksi Menggunakan LinkAja', async (I) => {
+  await I.login(process.env.EMAIL, process.env.PASSWORD);
+  await I.pilihOutlet(process.env.OUTLET);
+  await I.waitForVisible('Home', 100);
+  await I.tap('Automation Kemeja');
+  await I.tap('SIMPAN');
+  await I.tap('BAYAR');
+  await I.tap(process.env.PELANGGAN);
+  await I.tap('SELANJUTNYA');
+  await I.see('Pilih Metode Pembayaran');
+  await I.see('Pembayaran Digital');
+  total = await I.grabTextFrom('#spe.pos.rewash:id/tv_payment_subtotal')
+    .match(patt)
+    .join('');
+  await I.tap('CASH');
+  await I.tap('10k');
+  bayar = await I.grabTextFrom('#spe.pos.rewash:id/number_display')
+    .match(patt)
+    .join('');
+  await I.tap('SIMPAN');
+  await I.tap('KONFIRMASI');
+  kembalian = await I.grabTextFrom('#spe.pos.rewash:id/tvKembalian')
+    .match(patt)
+    .join('');
+  console.log(total, bayar, kembalian);
 });
 
 // Scenario('Gagal Login ketika memasukan email atau password yang salah', async (I) => {
